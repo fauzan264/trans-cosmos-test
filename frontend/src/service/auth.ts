08864d3a7b34b6f5f297424a4cd66e27ab53f40b
Fauzan <fauzan.c4@gmail.com>
2025-10-17 14:53:52 +0700

    feat: create service auth

---
+++ frontend/src/service/auth.ts
@@ -0,0 +1,43 @@
+import { IAuth } from "@/features/auth/types";
+import { axiosInstance } from "@/lib/axiosInstances";
+import snakecaseKeys from "snakecase-keys";
+
+const AUTH_URL = "/auth";
+
+export const register = ({ name, email, password }: IAuth) => {
+  return axiosInstance.post(
+    `${AUTH_URL}/register`,
+    snakecaseKeys({
+      name,
+      email,
+      password,
+    })
+  );
+};
+
+export const login = ({ email, password }: IAuth) => {
+  return axiosInstance.post(`${AUTH_URL}/login`, {
+    email,
+    password,
+  });
+};
+
+export const logout = ({ token }: { token: string }) => {
+  return axiosInstance.post(
+    `${AUTH_URL}/logout`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
+
+export const sessionLogin = ({ token }: { token: string }) => {
+  return axiosInstance.get(`${AUTH_URL}/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
