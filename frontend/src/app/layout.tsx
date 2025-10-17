e8a0ad832363dae059088ef99f14d31cf987c54e
Fauzan <fauzan.c4@gmail.com>
2025-10-17 16:08:03 +0700

    fix: update layout add auth provider, toast container, and navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "@/providers/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import Navbar from "@/components/ui/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>
+          <ToastContainer />
+          <Navbar />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
